15ba76116a1cb40cc92f1f35eaffdcc55507d5c1
Helder Guerreiro <helder@tretas.org>
2017-12-11 22:22:02 +0000

    Slightly better menu
    - Auto hide on mouse leave;
    - Click to close.

--- labs_django/static/js/main_menu.js
+++ labs_django/static/js/main_menu.js
@@ -7,20 +7,29 @@ $(function() {
     $("#header_menu").click( function() {
         if (visible) {
             $("#main_menu").hide();
+            $("#menu_back").hide();
             visible = false;
         } else {
             $("#main_menu").show();
+            $("#menu_back").show();
             visible = true;
         };
     });
 
-    $("#main_menu").mouseleave( function() {
-         setTimeout(function(){ 
-             if (!( $("#main_menu:hover").length )) {
-                $("#main_menu").hide();
-                visible = false;
-             };
-         }, 2000);
+    $("#menu_back").click( function() {
+        $("#main_menu").hide();
+        $("#menu_back").hide();
+        visible = false;
     });
-});
 
+    $("#main_menu").mouseleave( function() {
+        setTimeout(function(){
+            if (!( $("#main_menu:hover").length )) {
+                $("#main_menu").hide();
+                $("#menu_back").hide();
+                visible = false;
+            };
+        }, 2000);
+    });
+
+});
